a8e508c651f7943401b77bb484971ccb96bc46ed
Mohammad Rezki <159318071+mr22206@users.noreply.github.com>
2024-09-25 20:16:11 +0200

    Add files via upload

---
+++ Depannageinfo06/script.js
@@ -0,0 +1,6 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('bone_distance').addEventListener('click', function() {
+        window.location.href = '../distance/index-distance.html';
+    });
+});
