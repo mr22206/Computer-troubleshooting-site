9d1419f184c67d8832da2c55dbfea52112110f1f
mohammad <mohammad.rezki@viacesi.fr>
2025-02-14 21:03:42 +0100

    fix: configure routes for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Services from './pages/Services';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Computer-troubleshooting-site">
       <div className="App">
         <Header />
         <Routes>
